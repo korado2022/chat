7c32d55946030e887f358b16bd7108f6ddf57027
korado29 <korado_29@mail.ru>
2023-05-31 21:49:30 +0300

    My homework for lesson 5 is ready

---
+++ lesson_5/server.py
@@ -0,0 +1,104 @@
+"""Программа-сервер"""
+import logging
+from socket import *
+import sys
+import json
+import logs.config_server_log
+from config import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
+    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
+from utils import get_message, send_message
+
+
+
+#Инициализация логирования сервера.
+SERVER_LOGGER = logging.getLogger('server')
+
+def process_client_message(message):
+    '''
+    Обработчик сообщений от клиентов, принимает словарь -
+    сообщение от клиента, проверяет корректность,
+    возвращает словарь-ответ для клиента
+
+    :param message:
+    :return:
+    '''
+    SERVER_LOGGER.debug(f'Получено сообщение от клиента : {message}')
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Sergey':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
+    }
+
+
+def main():
+    '''
+    Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
+    Сначала обрабатываем порт:
+    server.py -p 8079 -a 192.168.1.2
+    :return:
+    '''
+
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = DEFAULT_PORT
+        if listen_port < 1024 or listen_port > 65535:
+            raise ValueError
+    except IndexError:
+        SERVER_LOGGER.debug('Не указан номер порта, будет подставлено значение по умолчанию 8079')
+        print('После параметра -\'p\' необходимо указать номер порта.')
+        sys.exit(1)
+    except ValueError:
+        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
+                               f'{listen_port}. Допустимы адреса с 1024 до 65535.')
+        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+
+    # Затем загружаем какой адрес слушать
+
+    try:
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = ''
+
+    except IndexError:
+        SERVER_LOGGER.debug('Не указан адрес, который будет слушать сервер, слушать будет все')
+        print('После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        sys.exit(1)
+
+    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+                       f'адрес с которого принимаются подключения: {listen_address}. '
+                       f'Если адрес не указан, принимаются соединения с любых адресов.')
+
+    # Готовим сокет
+    transport = socket(AF_INET, SOCK_STREAM)
+    transport.bind((listen_address, listen_port))
+
+    # Слушаем порт
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        try:
+            message_from_client = get_message(client)
+            SERVER_LOGGER.debug(f'Получено сообщение: {message_from_client}')
+            # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
+            response = process_client_message(message_from_client)
+            SERVER_LOGGER.info(f'Cформирован ответ клиенту: {response}')
+            send_message(client, response)
+            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от '
+                                f'клиента {client_address}. Соединение закрывается.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
