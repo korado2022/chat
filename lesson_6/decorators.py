7b902fb1e2821b08924d778472980a8d9d0e9c36
korado29 <korado_29@mail.ru>
2023-06-04 22:04:57 +0300

    My homework for lesson 6 is ready

---
+++ lesson_6/decorators.py
@@ -0,0 +1,29 @@
+import inspect
+import logging
+import sys
+import traceback
+import logs.config_server_log
+import logs.config_client_log
+
+
+if sys.argv[0].find('client') == -1:
+    # если не клиент то сервер!
+    LOGGER = logging.getLogger('server')
+else:
+    # ну, раз не сервер, то клиент
+    LOGGER = logging.getLogger('client')
+
+
+# Реализация в виде функции
+def log(func_log):
+    """Функция-декоратор"""
+    def wrap(*args, **kwargs):
+        """Обертка"""
+        ret = func_log(*args, **kwargs)
+        LOGGER.debug(f'Была вызвана функция {func_log.__name__} c параметрами {args}, {kwargs}. '
+                     f'Вызов из модуля {func_log.__module__}. Вызов из'
+                     f' функции {traceback.format_stack()[0].strip().split()[-1]}.'
+                     f'Вызов из функции {inspect.stack()[1][3]}')
+        return ret
+    return wrap
+
