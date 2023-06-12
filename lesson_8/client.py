b140555497921a5feedf13361b3a89023e793931
korado29 <korado_29@mail.ru>
2023-06-12 14:06:15 +0300

    My homework for lesson 8 is ready

---
+++ lesson_8/client.py
@@ -0,0 +1,223 @@
+"""Программа-клиент"""
+import logging
+import sys
+import json
+from socket import *
+import time
+import threading
+import logs.config_client_log
+from config import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT, SENDER, \
+    MESSAGE, MESSAGE_TEXT, DESTINATION, EXIT
+from errors import ReqFieldMissingError, ServerError, IncorrectDataRecivedError
+from utils import get_message, send_message
+from decorators import log
+
+
+# Инициализация клиентского логера
+LOGGER = logging.getLogger('client')
+
+
+@log
+def create_exit_message(account_name):
+    """Функция создаёт словарь с сообщением о выходе"""
+    message = f'Пользователь {account_name} вышел из чата.'
+    return {
+        ACTION: EXIT,
+        TIME: time.time(),
+        ACCOUNT_NAME: account_name,
+        MESSAGE_TEXT: message
+    }
+
+
+@log
+def message_from_server(sock, my_username):
+    """Функция - обработчик сообщений других пользователей, поступающих с сервера"""
+    while True:
+        try:
+            message = get_message(sock)
+            if ACTION in message and message[ACTION] == MESSAGE and \
+                    SENDER in message and DESTINATION in message \
+                    and MESSAGE_TEXT in message and message[DESTINATION] == my_username:
+                print(f'Получено сообщение от пользователя '
+                      f'{message[SENDER]}:\n{message[MESSAGE_TEXT]}')
+                LOGGER.info(f'Получено сообщение от пользователя '
+                            f'{message[SENDER]}:\n{message[MESSAGE_TEXT]}')
+            else:
+                LOGGER.error(f'Получено некорректное сообщение с сервера: {message}')
+        except IncorrectDataRecivedError:
+            LOGGER.error(f'Не удалось декодировать полученное сообщение.')
+        except (OSError, ConnectionError, ConnectionAbortedError,
+                ConnectionResetError, json.JSONDecodeError):
+            LOGGER.critical(f'Потеряно соединение с сервером.')
+            break
+
+@log
+def create_message(sock, account_name='anonymous'):
+    """
+    Функция запрашивает кому отправить сообщение и само сообщение,
+    и отправляет полученные данные на сервер
+    :param sock:
+    :param account_name:
+    :return:
+    """
+    to_user = input('Введите получателя сообщения: ')
+    message = input('Введите сообщение для отправки: ')
+    message_dict = {
+        ACTION: MESSAGE,
+        SENDER: account_name,
+        DESTINATION: to_user,
+        TIME: time.time(),
+        MESSAGE_TEXT: message
+    }
+    LOGGER.debug(f'Сформирован словарь сообщения: {message_dict}')
+    try:
+        send_message(sock, message_dict)
+        LOGGER.info(f'Отправлено сообщение для пользователя {to_user}')
+    except:
+        LOGGER.critical('Потеряно соединение с сервером.')
+        sys.exit(1)
+
+def print_help():
+    """Функция выводящая справку по использованию"""
+    print('Поддерживаемые команды:')
+    print('message - отправить сообщение. Кому и текст будет запрошены отдельно.')
+    print('help - вывести подсказки по командам')
+    print('exit - выход из программы')
+
+
+
+@log
+def user_interactive(sock, username):
+    """Функция взаимодействия с пользователем, запрашивает команды, отправляет сообщения"""
+    print_help()
+    while True:
+        command = input('Введите команду: ')
+        if command == 'message':
+            create_message(sock, username)
+        elif command == 'help':
+            print_help()
+        elif command == 'exit':
+            send_message(sock, create_exit_message(username))
+            print('Завершение соединения.')
+            LOGGER.info('Завершение работы по команде пользователя.')
+            # Задержка необходима, чтобы успело уйти сообщение о выходе
+            time.sleep(0.5)
+            break
+        else:
+            print('Команда не распознана, попробуйте снова. help - вывести поддерживаемые команды.')
+
+
+
+@log
+def create_presence(account_name='anonymous'):
+    """
+    Функция генерирует запрос о присутствии клиента
+    :param account_name:
+    :return:
+    """
+    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'anonymous'}}
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
+    return out
+
+@log
+def process_ans(message):
+    """
+    Функция разбирает ответ сервера
+    :param message:
+    :return:
+    """
+    LOGGER.debug(f'Получено сообщение от сервера: {message}')
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ReqFieldMissingError(RESPONSE)
+
+
+def main():
+    """Загружаем параметры командной строки"""
+    # client.py 192.168.1.2 8079 test_1
+
+    """Сообщаем о запуске"""
+    print('Консольный мессенджер. Клиентский модуль.')
+
+    try:
+        server_address = sys.argv[1]
+        server_port = int(sys.argv[2])
+        client_name = sys.argv[3]
+        if server_port < 1024 or server_port > 65535:
+            raise ValueError
+    except IndexError:
+        server_address = DEFAULT_IP_ADDRESS
+        server_port = DEFAULT_PORT
+        # client_name = 'anonymous'
+    except ValueError:
+        LOGGER.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}.'
+            f' Допустимы адреса с 1024 до 65535. Клиент завершается.')
+        sys.exit(1)
+    print(f'Пользователь {client_name}')
+    LOGGER.info(
+        f'Запущен клиент с параметрами: адрес сервера: {server_address}, '
+        f'порт: {server_port}, имя пользователя: {client_name}')
+
+    # Инициализация сокета и обмен
+    try:
+        transport = socket(AF_INET, SOCK_STREAM)
+        print(transport)
+        transport.connect((server_address, server_port))
+        message_to_server = create_presence(client_name)
+        send_message(transport, message_to_server)
+        answer = process_ans(get_message(transport))
+        print(answer)
+        LOGGER.info(f'Принят ответ от сервера {answer}')
+        print(f'Установлено соединение с сервером.')
+    except json.JSONDecodeError:
+        LOGGER.error('Не удалось декодировать полученную Json строку.')
+        sys.exit(1)
+    except ServerError as error:
+        LOGGER.error(f'При установке соединения сервер вернул ошибку: {error.text}')
+        sys.exit(1)
+    except ReqFieldMissingError as missing_error:
+        LOGGER.error(f'В ответе сервера отсутствует необходимое поле '
+                            f'{missing_error.missing_field}')
+        sys.exit(1)
+    except (ConnectionRefusedError, ConnectionError):
+        LOGGER.critical(
+            f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+            f'конечный компьютер отверг запрос на подключение.')
+        sys.exit(1)
+    else:
+        # Если соединение с сервером установлено корректно,
+        # запускаем клиентский процесс приёма сообщений
+        receiver = threading.Thread(target=message_from_server, args=(transport, client_name))
+        receiver.daemon = True
+        receiver.start()
+
+        # затем запускаем отправку сообщений и взаимодействие с пользователем.
+        user_interface = threading.Thread(target=user_interactive, args=(transport, client_name))
+        user_interface.daemon = True
+        user_interface.start()
+        LOGGER.debug('Запущены процессы')
+
+        # Watchdog основной цикл, если один из потоков завершён,
+        # то значит или потеряно соединение или пользователь
+        # ввёл exit. Поскольку все события обрабатываются в потоках,
+        # достаточно просто завершить цикл.
+        while True:
+            time.sleep(1)
+            if receiver.is_alive() and user_interface.is_alive():
+                continue
+            break
+
+
+if __name__ == '__main__':
+    main()
